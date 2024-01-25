c8d1f1b9229852d8fb081fb47905c8aa4af7f161
Brandon-Ritchie <ritchietrumpet@gmail.com>
2024-01-24 19:26:03 -0500

    Clean up PageLayout

--- src/lib/shared/PageLayout.tsx
+++ src/lib/shared/PageLayout.tsx
@@ -1,7 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 
-const PageLayout = () => {
+export default function PageLayout() {
   return (
     <div className="min-h-full">
       <Analytics />
@@ -11,47 +11,23 @@ const PageLayout = () => {
       </main>
     </div>
   );
-};
+}
 
-const NavBar = () => {
+function NavBar() {
   return (
     <div className="navbar w-full bg-primary">
-      <div className="flex-1"></div>
-
-      <nav className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div className="flex-none">
-            <button className="btn btn-square btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-5 w-5 stroke-secondary-content"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <ul className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-neutral p-2 shadow">
-            <Links />
-          </ul>
-        </div>
+      <div className="flex-1" />
+      <nav className="flex-none lg:hidden">
+        <DropdownNavMenu />
       </nav>
       <nav className="navbar-end hidden flex-none lg:flex">
-        <ul className="menu menu-horizontal px-1 text-secondary-content">
-          <Links />
-        </ul>
+        <NavMenu />
       </nav>
     </div>
   );
-};
+}
 
-const Links = () => {
+function Links() {
   const links = [
     { to: "/", label: "Home" },
     { to: "/profile", label: "Profile" },
@@ -68,6 +44,39 @@ const Links = () => {
       ))}
     </>
   );
-};
+}
 
-export default PageLayout;
+function DropdownNavMenu() {
+  return (
+    <div className="dropdown dropdown-end">
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-secondary-content"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+      <ul className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-neutral p-2 shadow">
+        <Links />
+      </ul>
+    </div>
+  );
+}
+
+function NavMenu() {
+  return (
+    <ul className="menu menu-horizontal px-1 text-secondary-content">
+      <Links />
+    </ul>
+  );
+}
